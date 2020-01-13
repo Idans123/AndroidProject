340e90e2ca27f0b1ed1560ef582af317a0f1f5ee
Idans123 <shoshani.idan@gmail.com>
2020-01-13 22:25:35 +0200

    basic using of sharedprefrences to save player name

--- app/src/main/java/com/example/balloonsworld/MainActivity.java
+++ app/src/main/java/com/example/balloonsworld/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.balloonsworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences sharedPreferences;
+    EditText playerNameET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences("storage",MODE_PRIVATE);
+        playerNameET = findViewById(R.id.playerNameET);
+        playerNameET.setText(sharedPreferences.getString("player_name",""));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("player_name",playerNameET.getText().toString());
+        editor.commit();
     }
 }
