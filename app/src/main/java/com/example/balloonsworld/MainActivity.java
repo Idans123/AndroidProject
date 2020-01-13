9d6ef0c1d00e9fa6557090c43ed811881223f637
Idans123 <shoshani.idan@gmail.com>
2020-01-13 22:45:24 +0200

    add alert dialog

--- app/src/main/java/com/example/balloonsworld/MainActivity.java
+++ app/src/main/java/com/example/balloonsworld/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.balloonsworld;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
         sharedPreferences = getSharedPreferences("storage",MODE_PRIVATE);
         playerNameET = findViewById(R.id.playerNameET);
         playerNameET.setText(sharedPreferences.getString("player_name",""));
+        Button exitBtn = findViewById(R.id.exitBtn);
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Exit")
+                        .setMessage("Are you sure you want to exit?")
+                        .setPositiveButton("Yes", new AlertDialogListener())
+                        .setNegativeButton("No",new AlertDialogListener())
+                        .show();
+            }
+        });
     }
 
     @Override
@@ -24,5 +40,18 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("player_name",playerNameET.getText().toString());
         editor.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+    private class AlertDialogListener implements DialogInterface.OnClickListener{
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            if(which == DialogInterface.BUTTON_POSITIVE){
+                finish();
+            }
+        }
     }
 }
