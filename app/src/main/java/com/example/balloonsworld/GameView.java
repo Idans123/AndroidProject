fd4b48ca12b1aba0babf39e6b171d98e7490d59c
Idans123 <shoshani.idan@gmail.com>
2020-01-20 18:49:26 +0200

    poc of score

--- app/src/main/java/com/example/balloonsworld/GameView.java
+++ app/src/main/java/com/example/balloonsworld/GameView.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
@@ -16,11 +17,19 @@ public class GameView extends View {
     private int canvasHeight;
     private int canvasWidth;
     private int balloonX;
+    private int coinX, coinY=0, coinSpeed=16;
+    private Paint coinPaint = new Paint();
+    private int score=0;
+    private boolean touch = false;
+
 
     public GameView(Context context) {
         super(context);
         ballon = BitmapFactory.decodeResource(getResources(),R.drawable.balloon);
         balloonX=canvasWidth/2 - ballon.getWidth()/2;
+
+        coinPaint.setColor(Color.YELLOW);
+        coinPaint.setAntiAlias(false);
 
     }
 
@@ -41,18 +50,34 @@ public class GameView extends View {
             balloonX=maxBallonX;
         }
         canvas.drawBitmap(ballon,balloonX,canvasHeight - ballon.getHeight()*2,null);
-    }
 
+        coinY+=coinSpeed;
+        if(hitCheker(coinX,coinY))
+        {
+            score+=10;
+            coinY+=1000;
+            System.out.println("Score: "+score);
+        }
+        if(coinY>canvasHeight){
+            coinY=0;
+            coinX=(int)Math.floor(Math.random()*(maxBallonX-minBallonX))+minBallonX;
+        }
+        canvas.drawCircle(coinX,coinY,25,coinPaint);
+        hitCheker(coinX,coinY);
+    }
+    public boolean hitCheker(int x, int y){
+        return(canvasHeight - ballon.getHeight()*2<y &&
+                (canvasHeight - ballon.getHeight()*2+ballon.getHeight())>y &&
+                x>ballon.getWidth() && x<ballon.getWidth()+balloonX);
+    }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-//        if(event.getAction() == MotionEvent.ACTION_POINTER_DOWN)
-//        {
-//           balloonX=(int)event.getX();
-//        }
+
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_MOVE:
                 balloonX=(int)event.getX();
+//                System.out.println(event.getX()+","+event.getY());
         }
         return true;
     }
