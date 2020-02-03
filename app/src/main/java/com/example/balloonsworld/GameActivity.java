0f1ca8994754e454d200e3e574f86aa4598a6573
Omer Ifrach <omer.ifrach1@gmail.com>
2020-02-03 13:22:53 +0200

    Game pause added

--- app/src/main/java/com/example/balloonsworld/GameActivity.java
+++ app/src/main/java/com/example/balloonsworld/GameActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
@@ -18,38 +19,11 @@ public class GameActivity extends AppCompatActivity {
     private Handler  handler= new Handler();
     private final static long interval=30;
     private Timer timer;
+    private AlertDialog menuDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        gameView = new GameView(this,1);
-        gameView.setListner(new GameView.GameEventListener() {
-            @Override
-            public void pauseGame() {
-                timer.cancel();
-                AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
-                builder.setTitle("Exit")
-                        .setMessage("Are you sure you want to exit?")
-                        .setPositiveButton("Yes", new AlertDialogListener())
-                        .setNegativeButton("No",new AlertDialogListener())
-                        .show();
-            }
-
-            @Override
-            public void resumeGame() {
-                timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                gameView.invalidate();
-                            }
-                        });
-                    }
-                },0,interval);
-            }
-        });
+        this.initGameView();
         setContentView(gameView);
 
         this.timer = new Timer();
@@ -65,6 +39,83 @@ public class GameActivity extends AppCompatActivity {
             }
         },0,interval);
     }
+    public void initGameView(){
+        gameView = new GameView(this,1);
+        gameView.setListner(new GameView.GameEventListener() {
+            @Override
+            public void pauseGame() {
+                timer.cancel();
+                final AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+
+                View pasueDialog=getLayoutInflater().inflate(R.layout.pause_game_menu,null);
+
+                final Button resumeBtn=pasueDialog.findViewById(R.id.resumeBtn);
+                final Button restartBtn=pasueDialog.findViewById(R.id.restartGameBtn);
+                final Button exitBtn=pasueDialog.findViewById(R.id.exitGameBtn);
+
+                resumeBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        resumeGame();
+                        menuDialog.dismiss();
+
+                    }
+                });
+
+                restartBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        initGameView();
+                        setContentView(gameView);
+                        resumeGame();
+                        menuDialog.dismiss();
+                    }
+                });
+                exitBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        exitGame();
+                    }
+                });
+
+                menuDialog= builder.setView(pasueDialog).show();
+            }
+
+            @Override
+            public void endGame() {
+                
+            }
+
+            public void exitGame(){
+                finish();
+            }
+
+            public void restartGame(){
+
+            }
+
+            @Override
+            public void resumeGame() {
+
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                gameView.invalidate();
+                            }
+                        });
+                    }
+                },0,interval);
+            }
+        });
+    }
+
+
+
+
     private class AlertDialogListener implements DialogInterface.OnClickListener{
         @Override
         public void onClick(DialogInterface dialog, int which) {
@@ -73,5 +124,7 @@ public class GameActivity extends AppCompatActivity {
             }
         }
     }
+
+
 
 }
