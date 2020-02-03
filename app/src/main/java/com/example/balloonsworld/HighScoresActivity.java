30b5dba16e8a83b2211892e8628a4a5c3c629127
Idans123 <shoshani.idan@gmail.com>
2020-02-03 13:41:41 +0200

    get highscore list from sharedPreferences

--- app/src/main/java/com/example/balloonsworld/HighScoresActivity.java
+++ app/src/main/java/com/example/balloonsworld/HighScoresActivity.java
@@ -1,5 +1,6 @@
 package com.example.balloonsworld;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -12,31 +13,31 @@ import java.util.List;
 
 public class HighScoresActivity extends AppCompatActivity {
     private List<HashMap<String, Object>> stomList;
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.high_scores_layout);
-
+        sharedPreferences = getSharedPreferences("storage",MODE_PRIVATE);
         ListView listView = findViewById(R.id.high_scores_list);
         stomList = new ArrayList<HashMap<String, Object>>();
-        setMockData();
+        getUsersFromSP();
         String [] from = {"index","name","points"};
         int [] ids = {R.id.index_high_score_tv, R.id.name_high_score_tv, R.id.points_high_score_tv};
         SimpleAdapter simpleAdapter = new SimpleAdapter(this,stomList,R.layout.high_score_cell,from,ids);
         listView.setAdapter(simpleAdapter);
     }
-    private void setMockData(){
+    private void getUsersFromSP(){
         HashMap<String,Object> stringToObjectMap = new HashMap<String, Object>();
-        stringToObjectMap.put("index","1.");
-        stringToObjectMap.put("name","David");
-        stringToObjectMap.put("points","100");
-        stomList.add(stringToObjectMap);
-
-        stringToObjectMap = new HashMap<String, Object>();;
-        stringToObjectMap.put("index","2.");
-        stringToObjectMap.put("name","Yosi");
-        stringToObjectMap.put("points","70");
-        stomList.add(stringToObjectMap);
-
+        for(int i=1;i<=10;i++){
+            if(sharedPreferences.contains("player_name"+i)&&sharedPreferences.contains("player_score"+i)){
+                stringToObjectMap.put("index",i+".");
+                stringToObjectMap.put("name",sharedPreferences.getString("player_name"+i,""));
+                stringToObjectMap.put("points",sharedPreferences.getString("player_score"+i,""));
+                stomList.add(stringToObjectMap);
+            }
+            stringToObjectMap = new HashMap<String, Object>();;
+        }
     }
 }
