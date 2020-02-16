c909cd1c2db25abe76fef9658d07bf4ae592f4f2
Idans123 <shoshani.idan@gmail.com>
2020-02-16 17:59:48 +0200

    add konfetti library and full imp

--- app/src/main/java/com/example/balloonsworld/HighScoresActivity.java
+++ app/src/main/java/com/example/balloonsworld/HighScoresActivity.java
@@ -1,6 +1,7 @@
 package com.example.balloonsworld;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,9 +14,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
+
 public class HighScoresActivity extends AppCompatActivity {
     private List<HashMap<String, Object>> stomList;
     private SharedPreferences sharedPreferences;
+    private KonfettiView konfettiView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,7 @@ public class HighScoresActivity extends AppCompatActivity {
         int [] ids = {R.id.index_high_score_tv, R.id.name_high_score_tv, R.id.points_high_score_tv};
         SimpleAdapter simpleAdapter = new SimpleAdapter(this,stomList,R.layout.high_score_cell,from,ids);
         listView.setAdapter(simpleAdapter);
+        startKonfetti();
     }
     private void setSPMockData(){
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -64,5 +71,18 @@ public class HighScoresActivity extends AppCompatActivity {
             }
             stringToObjectMap = new HashMap<String, Object>();;
         }
+    }
+    private void startKonfetti(){
+        konfettiView =  findViewById(R.id.konfettiView);
+        konfettiView.build()
+                .addColors(Color.YELLOW,Color.GREEN,Color.MAGENTA)
+                .setDirection(0.0,359.0)
+                .setSpeed(1f,5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.RECT,Shape.CIRCLE)
+                .addSizes(new Size(12,5))
+                .setPosition(-50,konfettiView.getWidth()+50f,-50f,-50f)
+                .streamFor(300,5000L);
     }
 }
